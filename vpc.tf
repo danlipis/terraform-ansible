ee286c134f0579464f1d9c63f21388ded37319f1
Daniel Bastos <dbastos@lalala.com>
2019-09-03 17:49:57 +0000

    Alterado  o nome do servidor

--- vpc.tf
+++ vpc.tf
@@ -6,10 +6,17 @@ resource "aws_default_security_group" "default" {
   vpc_id = "${aws_vpc.mainvpc.id}"
 
   ingress {
-    protocol  = -1
-    self      = true
-    from_port = 0
-    to_port   = 0
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
