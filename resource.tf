5b964e623fc661c14ce5934e506b3a18742721bc
Daniel Bastos <dbastos@lalala.com>
2019-09-04 17:14:27 +0000

    Adicionado linha para criar 2 servidores

--- resource.tf
+++ resource.tf
@@ -1,12 +1,13 @@
 resource "aws_instance" "server" {
 
+ count = 2
  ami = "ami-0b898040803850657"
  instance_type = "t2.micro"
  key_name = "keyvirginia"
  security_groups = ["${aws_security_group.mysecgroup.name}"]
 
  tags = {
-   Name = "ServerJenkins"
+   Name = "Server.${count.index}"
  }
 }
 
