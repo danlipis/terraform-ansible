6133eb87bf9e895160fb7769706a76d165eef1a7
Daniel Bastos <dbastos@lalala.com>
2019-09-04 13:01:47 +0000

    Adicionado linha para referir o novo grupo de segurança criado personalizado

--- resource.tf
+++ resource.tf
@@ -3,10 +3,10 @@ resource "aws_instance" "server" {
  ami = "ami-0b898040803850657"
  instance_type = "t2.micro"
  key_name = "keyvirginia"
-
+ security_groups = ["${aws_security_group.mysecgroup.name}"]
 
  tags = {
-   Name = "serverjenkins"
+   Name = "ServerJenkins"
  }
 }
 
