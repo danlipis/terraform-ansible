70fc5e0e4ccc87ce389bec57323cabfcb868e612
Daniel Bastos <dbastos@lalala.com>
2019-09-05 18:31:24 +0000

    Adicionado liberacao da porta 9990 e 10090

--- vpc.tf
+++ vpc.tf
@@ -7,8 +7,30 @@ resource "aws_security_group" "mysecgroup" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
-    from_port = 22
+    from_port = 8180
+    to_port = 8180
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 9990
+    to_port = 9990
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port = 10090
+    to_port = 10090
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+  from_port = 22
     to_port = 22
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
