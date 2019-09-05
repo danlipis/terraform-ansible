014ca501de2691b46f87d2f16d10ffe407965703
Daniel Bastos <dbastos@lalala.com>
2019-09-05 12:13:47 +0000

    Adicionado linas de chamada para variaveis de numero de instance e nome para instancia

--- resource.tf
+++ resource.tf
@@ -1,13 +1,13 @@
 resource "aws_instance" "server" {
 
- count = 2
+ count = "${var.instance_count}"
  ami = "ami-0b898040803850657"
  instance_type = "t2.micro"
  key_name = "keyvirginia"
  security_groups = ["${aws_security_group.mysecgroup.name}"]
 
  tags = {
-   Name = "Server.${count.index}"
+   Name = "Server.${element(var.instance_tags, count.index)}"
  }
 }
 
